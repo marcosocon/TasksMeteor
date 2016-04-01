3ef67c15523b337290cdd0111099ce66a4c6743a
marcosocon <oconmarcos97@gmail.com>
2016-04-01 13:39:28 -0300

    added feature to make a task public or private

--- tasks-meteor.js
+++ tasks-meteor.js
@@ -2,8 +2,8 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
 
-  Meteor.subscribe("tasks")
-
+  Meteor.subscribe("tasks");
+//BODY JS - helpers.
   Template.body.helpers({
     tasks:function(){
       if (Session.get('hideFinished')) {
@@ -17,16 +17,7 @@ if (Meteor.isClient) {
       return Session.get('hideFinished');
     }
   });
-
-  Template.task.events({
-    "click .toggle-checked":function(){
-      Meteor.call("updateTask", this._id, !this.checked);
-    },
-    "click .delete":function(){
-      Meteor.call("deleteTask", this._id);
-    },
-  })
-
+//BODY JS - events.
   Template.body.events({
     'submit .new-task': function(event){
       var title = event.target.title.value;
@@ -44,7 +35,36 @@ if (Meteor.isClient) {
       Session.set('hideFinished', event.target.checked);
     }
   });
+//TASK JS - helpers.
+  Template.task.helpers({
+    isOwner: function(){
+      if (this.owner === Meteor.userId()){
+        return true
+      }
+      else{
+        return false
+      }
+    }
+  });
 
+  Template.task.events({
+    "click .toggle-private":function(){
+      Meteor.call("setPrivate", this._id, !this.private);
+    }
+  });
+
+//Task JS - events
+  Template.task.events({
+    "click .toggle-checked":function(){
+      Meteor.call("updateTask", this._id, !this.checked);
+    },
+    "click .delete":function(){
+      Meteor.call("deleteTask", this._id);
+    },
+  });
+
+
+//ACCOUNTS-UI - config.
   Accounts.ui.config({
     passwordSignupFields : "USERNAME_ONLY"
   });
@@ -56,19 +76,37 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish("tasks", function(){
-    return Tasks.find();
-  })
+    return Tasks.find({
+      $or: [
+      {private: { $ne:true }},
+      {owner: this.userId}
+      ]
+    });
+  });
 }
 //Secure meteor methods.
 Meteor.methods({
   addTask: function(title){
     Tasks.insert({
       title:title,
-      createdAt: new Date()
+      createdAt: new Date(),
+      owner: Meteor.userId()
       });
   },
   deleteTask: function(id){
     Tasks.remove(id);
+
+  },
+  setPrivate:function(id, private){
+    var task = Tasks.findOne(id);
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("Access denied!");
+    }
+    Tasks.update(id, {
+      $set:{
+        private: private
+      }
+    });
 
   },
   updateTask: function(id, checked){
